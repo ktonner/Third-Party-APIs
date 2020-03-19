26cfd75aa37fa9820ee6f906b4b8ed21bc9bd66c
Kathleen Tonner <kathleentonner@gmail.com>
2020-03-19 18:02:48 -0400

    Set class for rows by hour

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 
-//put the date at the top
+//put the current date at the top
 var currentDay = $("#currentDay")
 currentDay.text(moment().format('MMMM Do YYYY'))
 
+//declaring current hour as a variable 
+let currentHour = moment().hour()
+
+
+//Setting colors of all rows
+//loop through all rows
+$("textarea").each(function(){
+//conditional if the hour has passed
+if(currentHour > $(this).data("value")){
+    $(this).addClass("past")
+}
+//if current hour
+else if (currentHour == $(this).data("value")){
+    $(this).addClass("present")
+}
+//else it's the future
+else {
+    $(this).addClass("future")
+}
+})
+
+//Save button click
+$(".saveBtn").on("click", function(){
+    
+})
